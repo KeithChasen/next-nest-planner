da1cc096e4ecf6bd0e533d200bbb25954c6f7b68
keithchasen <keithchasen89@gmail.com>
2024-10-24 21:38:40 +0200

    basic set up main file of nest

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+
+	//prefix "/api" for all routes in the application
+	//localhost:4200/api/*
+	app.setGlobalPrefix('api');
+	app.use(cookieParser());
+	app.enableCors({
+		origin: ['http://localhost:3001'],
+		credentials: true,
+		exposedHeaders: 'set-cookie'
+	});
+
 	await app.listen(4200);
 }
 bootstrap();
